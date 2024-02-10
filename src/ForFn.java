913c11bf2cfd5d36720ba0298bcc69023b63d021
xzz2021 <zipeng.2016@gmail.com>
2024-02-10 20:53:54 +0800

    循环功能案例

---
+++ src/ForFn.java
@@ -0,0 +1,179 @@
+// 循环实现 功能 案例
+
+public class ForFn {
+    public static void main(String[] args) {
+
+    }
+//    1.打印矩形
+    public static void printRectangle(int row, int col) {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+//    2.打印直平行三角形
+    public static void printTriangle(int row) {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+//    3.打印等腰三角形
+    public static void printIsoscelesTriangle(int row) {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < row - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+//    4.打印菱形
+    public static void printDiamond(int row) {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < row - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = row - 2; i >= 0; i--) {
+            for (int j = 0; j < row - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+//    5.打印九九乘法表
+    public static void printMultiplicationTable() {
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + "*" + i + "=" + (i * j) + " ");
+            }
+            System.out.println();
+        }
+    }
+//    6.打印等边三角形
+    public static void printEquilateralTriangle(int row) {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < row - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    //    7.打印空心菱形
+    public static void printHollowDiamond(int row) {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < row - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                if (j == 0 || j == 2 * i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        for (int i = row - 2; i >= 0; i--) {
+            for (int j = 0; j < row - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                if (j == 0 || j == 2 * i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+//    8. 百钱买百鸡
+    public static void buyChicken() {
+        for (int i = 0; i <= 20; i++) {
+            for (int j = 0; j <= 33; j++) {
+                int k = 100 - i - j;
+                if (i * 5 + j * 3 + k / 3 == 100 && k % 3 == 0) {
+                    System.out.println("公鸡：" + i + " 母鸡：" + j + " 小鸡：" + k);
+                }
+            }
+        }
+    }
+//    9. 斐波那契数列
+    public static void fibonacci(int n) {
+        int a = 1;
+        int b = 1;
+        int c = 0;
+        for (int i = 0; i < n; i++) {
+            System.out.print(a + " ");
+            c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+//    10. 水仙花数
+    public static void narcissisticNumber() {
+        for (int i = 100; i < 1000; i++) {
+            int a = i / 100;
+            int b = i / 10 % 10;
+            int c = i % 10;
+            if (i == a * a * a + b * b * b + c * c * c) {
+                System.out.println(i);
+            }
+        }
+    }
+//    11. 完数
+    public static void perfectNumber() {
+        for (int i = 1; i < 1000; i++) {
+            int sum = 0;
+            for (int j = 1; j < i; j++) {
+                if (i % j == 0) {
+                    sum += j;
+                }
+            }
+            if (sum == i) {
+                System.out.println(i);
+            }
+        }
+    }
+//    12. 1000以内的质数
+    public static void primeNumber() {
+        for (int i = 2; i < 1000; i++) {
+            boolean flag = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                System.out.println(i);
+            }
+        }
+    }
+//    13. 阶乘
+    public static void factorial(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        System.out.println(result);
+    }
+}
